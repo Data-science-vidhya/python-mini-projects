5c801d3f1afff856e1d669793f466cbd91808cd8
machine-learnig-ai <65960477+machine-learnig-ai@users.noreply.github.com>
2020-05-27 14:13:27 +0530

    Add files via upload

---
+++ send Whatsapp message/whatsapp message.py
@@ -0,0 +1,7 @@
+import pywhatkit as kit
+# pip install pywhatkit
+kit.sendwhatmsg("+9191********", "I Love machine_learning.ai",14,10)
+
+# "+9191********": Receiver Number
+# "I Love machine_learning.ai": Message you want to send
+# Will open web.whatsapp.com at 14:09 and message will be sent at exactly 14:10
